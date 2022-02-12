d23a9dfd15cf15a462ec3cec65080c7626368f5e
abhijit <abhijit.das4@outlook.com>
2022-02-12 16:54:50 +0530

    csv file added

--- LRegression.py
+++ LRegression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-companies = pd.read_csv('/usr/src/app/1000_Companies.csv')
+companies = pd.read_csv('/data/1000_Companies.csv')
 X = companies.iloc[:, :-1].values
 y = companies.iloc[:, 4].values
 
@@ -21,4 +21,4 @@ y_pred = pd.DataFrame(y_pred)
 y_test=pd.DataFrame(y_test, columns=['Original'])
 y_test['Prediction']=y_pred
 
-y_test.to_csv('/usr/src/app/Result.csv',index=False)
+y_test.to_csv('/output/Result.csv',index=False)
